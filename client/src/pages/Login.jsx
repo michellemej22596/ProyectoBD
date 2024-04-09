d55f4b128e7529f931007290130702f56d5a1bb1
michellemej22596 <mej22596@uvg.edu.gt>
2024-04-09 13:56:38 -0600

    New design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,69 +1,75 @@
 import React from 'react';
-import { useState } from 'react'
-import { md5 } from 'js-md5'
-import { PropTypes } from 'prop-types'
-import Button from '../components/Button'
-import Input from '../components/Input'
-import '../styles/Login.css'
+import { useState } from 'react';
+import { md5 } from 'js-md5';
+import { PropTypes } from 'prop-types';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import '../styles/Login.css';
 
 const Login = ({ setToken, navigate }) => {
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
-
-  const [errorMessage, setErrorMessage] = useState('')
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [errorMessage, setErrorMessage] = useState('');
 
   const setValue = (name, value) => {
     switch(name) {
       case 'username':
-        setUsername(value)
-        break
+        setUsername(value);
+        break;
       case 'password':
-        setPassword(value)
-        break
+        setPassword(value);
+        break;
+      default:
+        break;
     }
-  }
+  };
 
   const handleSubmit = async () => {
-    const body = { }
-    body.username = username
-    body.password = md5(password) 
+    const body = {
+      username: username,
+      password: md5(password)
+    };
     const fetchOptions = {
       method: 'POST',
       body: JSON.stringify(body),
       headers: {
         'Content-Type': 'application/json'
       }
-    }
-    const response = await fetch('http://127.0.0.1:5000/login/', fetchOptions)
-    const { access_token } = await response.json()
+    };
+    const response = await fetch('http://127.0.0.1:5000/login/', fetchOptions);
+    const { access_token } = await response.json();
     if (response.ok) {
-      console.log('success! token is: ', access_token)
-      setToken(access_token)
-      navigate('/')
-      return
+      console.log('success! token is: ', access_token);
+      setToken(access_token);
+      navigate('/');
+      return;
     }
-    setErrorMessage('Incorrect user or password')
-  }
+    setErrorMessage('Incorrect user or password');
+  };
 
   return (
     <aside className="login">
-      <h1 className="title">Welcome!</h1>
+      <div className="logo-container">
+        <img src="../images/logo.avif" alt="Logo del Restaurante" className="login-logo" />
+      </div>
+      <h1 className="title">¡Bienvenido a Winery!</h1>
+      <p className="welcome-message">Disfruta de la experiencia culinaria más auténtica.</p>
       {
-        errorMessage !== '' ? (
+        errorMessage !== '' && (
           <div className='error-message' onClick={() => setErrorMessage('')}>
             {errorMessage}
           </div>
-        ) : null
+        )
       }
-      <Input placeholder="your username here" label="Username" type="text" value={username} onChange={(value) => setValue('username', value)} />
-      <Input label="Password" type="password" value={password} onChange={(value) => setValue('password', value)}/>
-      <Button text="Login" onClick={handleSubmit} />
+      <Input placeholder="Tu nombre de usuario" label="Usuario" type="text" value={username} onChange={(value) => setValue('username', value)} />
+      <Input label="Contraseña" type="password" value={password} onChange={(value) => setValue('password', value)}/>
+      <Button text="Iniciar sesión" onClick={handleSubmit} />
     </aside>
-  )
-}
+  );
+};
 
 Login.propTypes = {
   setToken: PropTypes.func.isRequired,
-}
+};
 
-export default Login
+export default Login;
