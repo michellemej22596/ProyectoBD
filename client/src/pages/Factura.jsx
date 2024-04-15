6ab7a1553fbe616d6e1b4e2a4bd1bf611893a124
Silviaillescas <silviaillesca402@gmail.com>
2024-04-14 21:14:04 -0600

    Estructura de PDF

--- client/src/pages/Factura.jsx
+++ client/src/pages/Factura.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
-import jsPDF from 'jspdf'; // Importa jsPDF
+import jsPDF from 'jspdf';
 
 function Factura() {
   const [tableNumber, setTableNumber] = useState('');
-  const [orderDetails, setOrderDetails] = useState(null);
+  const [orderDetails, setOrderDetails] = useState([]);
   const [error, setError] = useState('');
 
   const fetchOrderDetailsByTable = async (tableNumber) => {
-    const response = await fetch(`http://localhost:3000/orders/table/${tableNumber}`);
-    const data = await response.json();
-    if (response.ok) {
+    try {
+      const response = await fetch(`http://localhost:3000/orders/table/${tableNumber}`);
+      if (!response.ok) {
+        throw new Error('Pedido no encontrado o error en la solicitud');
+      }
+      const data = await response.json();
       setOrderDetails(data);
-    } else {
-      setError('Pedido no encontrado o error en la solicitud');
+    } catch (error) {
+      setError(error.message);
     }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setError('');
     if (!tableNumber) {
       setError('Por favor ingrese un número de mesa válido');
       return;
@@ -28,45 +32,54 @@ function Factura() {
   const generatePdf = () => {
     const doc = new jsPDF();
     const pageWidth = doc.internal.pageSize.getWidth();
-    let yPosition = 20; // Iniciar posición Y después del título
-  
-    // Centrar título
-    const title = `Pedido para la Mesa: ${tableNumber}`;
-    const titleWidth = doc.getStringUnitWidth(title) * doc.internal.getFontSize() / doc.internal.scaleFactor;
-    const titleOffset = (pageWidth - titleWidth) / 2;
-    doc.text(title, titleOffset, 10); // Posición Y fija para el título
-  
-    orderDetails.forEach((item, index) => {
-      const itemText = `${item.name} - ${item.quantity} unidades - Descripción: ${item.description} - Precio Total: $${item.totalitemprice}`;
-      const textWidth = doc.getStringUnitWidth(itemText) * doc.internal.getFontSize() / doc.internal.scaleFactor;
-      const margin = 20;
-  
-      // Verifica si el texto es más largo que el ancho de la página con márgenes
-      if (textWidth > (pageWidth - (margin * 2))) {
-        const splitText = doc.splitTextToSize(itemText, pageWidth - (margin * 2));
-        splitText.forEach((line, lineIndex) => {
-          doc.text(line, margin, yPosition + (10 * lineIndex));
-        });
-        yPosition += (10 * splitText.length); // Aumenta Y por cada línea de texto dividido
-      } else {
-        // Si el texto entra en una sola línea, centra el texto horizontalmente
-        const textOffset = (pageWidth - textWidth) / 2;
-        doc.text(itemText, textOffset, yPosition);
-        yPosition += 10; // Aumenta Y por una línea
-      }
-    });
-  
-    doc.save(`Pedido_Mesa_${tableNumber}.pdf`);
+    
+    // Ruta de la imagen desde la carpeta public
+    const imagePath = '/images/Logo.png'; // Asegúrate de que el nombre del archivo y la ruta son correctos
+
+    fetch(imagePath)
+      .then(res => res.blob())
+      .then(blob => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const base64data = reader.result;
+          
+          // Añadir imagen
+          doc.addImage(base64data, 'PNG', pageWidth / 2 - 30, 15, 60, 30);
+          
+          let yPosition = 60; // La posición Y debajo de la imagen
+
+          // Encabezado del PDF
+          doc.setFontSize(18);
+          const title = `Factura para la Mesa: ${tableNumber}`;
+          doc.text(title, pageWidth / 2, 50, { align: 'center' });
+
+          // Subtítulo y detalles
+          doc.setFontSize(12);
+          doc.text("Detalles del Pedido:", 14, yPosition);
+          yPosition += 10;
+
+          doc.setFontSize(10);
+          orderDetails.forEach((item, index) => {
+            const itemText = `${item.name} - ${item.quantity}x - Descripción: ${item.description} - Precio Total: $${item.totalPrice}`;
+            doc.text(itemText, 14, yPosition);
+            yPosition += 10;
+          });
+
+          doc.save(`Factura_Mesa_${tableNumber}.pdf`);
+        };
+        reader.readAsDataURL(blob);
+      })
+      .catch(err => {
+        console.error('Error al cargar la imagen:', err);
+      });
   };
-  
-  
 
   return (
     <div>
       <h1>Impresión del Pedido</h1>
       <form onSubmit={handleSubmit}>
         <input
-          type="text"
+          type="number"
           value={tableNumber}
           onChange={(e) => setTableNumber(e.target.value)}
           placeholder="Ingrese número de mesa"
@@ -74,13 +87,13 @@ function Factura() {
         <button type="submit">Mostrar Pedido</button>
       </form>
       {error && <p>{error}</p>}
-      {orderDetails && (
+      {orderDetails.length > 0 && (
         <div>
           <h2>Detalles del Pedido para la Mesa: {tableNumber}</h2>
           {orderDetails.map((detail, index) => (
             <div key={index}>
               <p>{detail.name} - {detail.quantity} unidades - {detail.description}</p>
-              <p>Precio unitario: ${detail.price} - Precio total: ${detail.totalitemprice}</p>
+              <p>Precio unitario: ${detail.price} - Precio total: ${detail.totalPrice}</p>
             </div>
           ))}
           <button onClick={generatePdf}>Generar PDF</button>
